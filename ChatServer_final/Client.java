5deda8dbb36827572ca526b8af717974232fbbed
Eokane <32264963+Eokane@users.noreply.github.com>
2017-11-07 10:38:16 +0000

    Create Client.java

---
+++ ChatServer_final/Client.java
@@ -0,0 +1,26 @@
+package server;
+
+//gets the ID of the client
+public class Client {
+
+	String name = "";
+
+	int id = 0;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
