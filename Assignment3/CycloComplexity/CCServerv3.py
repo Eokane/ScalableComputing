dd09e30ea5bc191e8cb7332801203705e975dd17
Eokane <Eiokane@tcd.ie>
2017-12-05 21:07:50 +0000

    calculating total complexity

--- Assignment3/CycloComplexity/CCServerv3.py
+++ Assignment3/CycloComplexity/CCServerv3.py
@@ -7,149 +7,101 @@ from git import Repo
 import os
 import json
 
-import pandas as pd
-import datetime
-import configparser
-
-
-
 
 #List of files to calculate the complexity
 Files = ['file1', 'file2', 'file3', 'file4', 'file5','file6','file7','file8','file9','file10']
 
 class MainHandler(tornado.web.RequestHandler):
 
-    def json_headers(self):
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT')
+   def json_headers(self):
+       self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT')
 
-    def returnData(self, data):
-        self.json_headers()
-        self.write(json_encode(data))
-        self.finish()
+   def returnData(self, data):
+       self.json_headers()
+       self.write(json_encode(data))
+       self.finish()
+
 
 class CCHandler(MainHandler):
-    @tornado.web.asynchronous
-    @gen.engine
-    #Retreive the information (itertion); File1 to file5 are found on localhost:4444 then spits out done
-    def get(self):
-          print ('Debug - get method')
+   @tornado.web.asynchronous
+   @gen.engine
+   #Retreive the information (itertion); File1 to file5 are found on localhost:4444 then spits out done
+   def get(self):
+       print ('Debug - get method')
 
-          if master.serverReady == False:
-              commit_number = 'Server is not Ready'
-          elif len(master.commits) == 0:
-              commit_number = 'Done'
-              # print (master.jobComplexity)
-              #print("Total Complexity = ", master.cc_total)
-              if not master.finish:
-                  master.finish=True
-                  master.finishedUP()
-          else:
-              commit = master.commits.pop(0)
-              commit_number = commit.hexsha
-          self.returnData(commit_number)
+       if len(master.commits) == 0:
+           commit_number = 'Done'
+           print ('Done. TotalTime (seconds):', int(master.jobComplexity/1000000))
+       else:
+           commit = master.commits.pop(0)
+           commit_number = commit.hexsha
+       self.returnData(commit_number)
 
-            # commits_tbd = master.commits
-        # if len(commits_tbd) == 0:
-        #     self.returnData('Done')
-        # else:
-        #     recent_commit = commits_tbd.pop(0)
-        #     self.returnData(recent_commit.hexhsa)
-class Initiate_Handler(MainHandler):
-
-        @tornado.web.asynchronous
-        @gen.engine
-        def get(self):
-            print('ReadyHandler - ready')
-            cc.serverReady = True
-            cc.StartWorkTime = datetime.datetime.now()
-            self.returnData('Ready to start!')
-
-class Results_Handler(MainHandler):
-        @tornado.web.asynchronous
-        @gen.engine
-        def get(self):
-            print('ResultsHandler - get ')
-            self.returnData(cc.resultsDb)
-
-def post(self):
-        #msg = json.loads(self.request.body.decode('utf-8'))
-        msg = json_decode(self.request.body)
-        commit = msg['commit']
-        complexity = msg['complexity']
-        duration = msg['duration']
-        clientName = msg['clientName']
+           # commits_tbd = master.commits
+       # if len(commits_tbd) == 0:
+       #     self.returnData('Done')
+       # else:
+       #     recent_commit = commits_tbd.pop(0)
+       #     self.returnData(recent_commit.hexhsa)
 
 
+   def post(self):
 
-        cc.resultsDb.append((commit, complexity, duration, clientName))
-
-        self.finish("Result received")
-        #print (msg['commit'])
-        #print (msg['complexity'])
-
-       # print ('Debug - post method: {}'.format(msg))
-
-        #master.cc_total += int(msg['complexity'])
-
-        #print(type(msg))
-
-
-        # commit = str(msg['commit'])
-        # complexity = msg['complexity']
-        #
-        # print(commit)
-        # print (type(commit ))
-        #
-        # self.jobComplexity[commit] = complexity
-        # self.finish("Result Received")
-
+       msg = self.request.body
+       msg = json_decode(msg)
+       print ('Debug - post method: {}'.format(msg))
+       # commit = str(msg['commit'])
+       # complexity = msg['complexity']
+       #
+       # print(commit)
+       # print (type(commit ))
+       #
+       # self.jobComplexity[commit] = complexity
+       # self.finish("Result Received")
+       ClientTime = int(msg['totaltime'])
+       master.jobComplexity = master.jobComplexity + ClientTime
 
 make_app = tornado.web.Application([
-    (r"/", CCHandler),
-    (r"/results", Results_Handler),
-    (r"/ready", Initiate_Handler)
-    ])
+   (r"/", CCHandler),
+   ])
 
 
 class Master:
 
-    def __init__(self):
+   def __init__(self):
 
-        self.gitrepo = "https://github.com/DLTK/DLTK"
-        self.repo_dir = 'c:\\CycloComplx\\Server'
-        self.repo = None
-        self.cc_total = 0
+       self.gitrepo = "https://github.com/DLTK/DLTK"
+       self.repo_dir = 'C:\\scalable\\CycloServer'
+       self.repo = None
 
-        self.clone_repo(self.repo_dir)
-        assert not self.repo.bare
-        print(self.repo)
+       self.clone_repo(self.repo_dir)
+       assert not self.repo.bare
+       print(self.repo)
 
-        self.commits = list(self.repo.iter_commits('master'))
-        #foo = repo.iter_commits('master')
-        #for i in foo:
-         #   print (i)
-        print (self.commits)
-        self.jobComplexity = {}
+       self.commits = list(self.repo.iter_commits('master'))
+       #foo = repo.iter_commits('master')
+       #for i in foo:
+        #   print (i)
+       print (self.commits)
+       self.jobComplexity = 0
 
-    def clone_repo(self, repo_dir):
-        # if repo root doesnt exists create it
-        if not os.path.exists(repo_dir):
-            os.makedirs(repo_dir)
+   def clone_repo(self, repo_dir):
+       # if repo root doesnt exists create it
+       if not os.path.exists(repo_dir):
+           os.makedirs(repo_dir)
 
-        # if repo root is empty, fill it
-        if not os.listdir(repo_dir):
-            Repo.clone_from(self.gitrepo, repo_dir)
-            self.repo = Repo(repo_dir)
-        else:
-            self.repo = Repo(repo_dir)
+       # if repo root is empty, fill it
+       if not os.listdir(repo_dir):
+           Repo.clone_from(self.gitrepo, repo_dir)
+           self.repo = Repo(repo_dir)
+       else:
+           self.repo = Repo(repo_dir)
 
 
 
 if __name__ == "__main__":
 
+   master = Master()
 
-
-    master = Master()
-
-    make_app.listen(4444)
-    IOLoop.instance().start()
+   make_app.listen(4444)
+   IOLoop.instance().start()
--- Assignment3/CycloComplexity/CC_Client.py
+++ Assignment3/CycloComplexity/CC_Client.py
@@ -9,133 +9,105 @@ import os
 from git import Repo
 import radon
 from radon.complexity import cc_rank, cc_visit
-import numpy as np
 import datetime
-import configparser
-CCAddress = "http://localhost:4444"
-
-
-class MainHandler(tornado.web.RequestHandler):
-#
-    def json_headers(self):
-       self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT')
-#
-#
-#
-#class CCHandler(MainHandler):
-#    def get(self):
-#        while True:
-#            reply = requests.get(CCAddress)
-#            job = reply.text
-#            if job == 'Done':
-#                break
-#
-#            msg = reply.text + 'completed.'
-#
-#            reply = requests.post('http://localhost:4444', data='{}'.format(msg))
-#
-#
-#
-#make_app = tornado.web.Application([
-#    (r"/Start", CCHandler),
-#])
-#
 
 def calculate_cc_complexity(files):
-    """ calculate cc for all files in current commit """
-    cc_tot = 0
-    for file in files:
-        with open(file) as f:
-            data = f.read()
-            try:
-                cc = radon.complexity.cc_visit(data)
-                cc_tot = 0
-                for cc_item in cc:
-                    cc_tot += cc_item.complexity
-                    # print("complexity = ", cc_item.complexity)
-            except Exception as err:
-                #print("ERROR: could not calculate cc for file {0} in commit {1}".format(file, commit_number))
-                print(err)
-                cc_tot = 0
-    # return total complexity of all files
-    return cc_tot
-
-
-
+   """ calculate cc for all files in current commit """
+   cc_tot = 0
+   for file in files:
+       with open(file) as f:
+           data = f.read()
+           try:
+               cc = radon.complexity.cc_visit(data)
+               
+               for cc_item in cc:
+                   cc_tot += cc_item.complexity
+                   # print("complexity = ", cc_item.complexity)
+           except Exception as err:
+               pass
+               # print("ERROR: could not calculate cc for file {0} in commit {1}".format(file, commit_number))
+               #print(err)
+   # return total complexity of all files
+   return cc_tot
 
 
 def calculateComplexity(f):
-    length_of_time = randint(1, 10)
-    #time.sleep(length_of_time)
-    print ('Complexity of file {} is {}'.format(f, length_of_time))
-    return length_of_time
+   length_of_time = randint(1, 10)
+   # time.sleep(length_of_time)
+   print('Complexity of file {} is {}'.format(f, length_of_time))
+   return length_of_time
+
 
 def update_files(repo_dir):
-    files = []
+   files = []
 
-    for (dirpath, dirnames, filenames) in os.walk(repo_dir):
-        for filename in filenames:
-            if '.py' in filename:
-                dirpath = dirpath.replace("\\", "/")
-                files.append(dirpath + '/' + filename)
-    return files
+   for (dirpath, dirnames, filenames) in os.walk(repo_dir):
+       for filename in filenames:
+           if '.py' in filename:
+               dirpath = dirpath.replace("\\", "/")
+               files.append(dirpath + '/' + filename)
+   return files
 
-    #cc_files = {file: None for file in self.files}
-    
-    
+   # cc_files = {file: None for file in self.files}
+
+
 if __name__ == "__main__":
-    # not using REST...yet(?)
-    # application.listen(8887)
-    # IOLoop.instance().start()
+   # not using REST...yet(?)
+   # application.listen(8887)
+   # IOLoop.instance().start()
 
-    #Setting up things
-    ClientName = 'Client_{}'.format(randint(1, 200))
-    print('This is {}'.format(ClientName))
-    git_repo = "https://github.com/DLTK/DLTK"
-    repo_dir = os.path.join("c:\\CycloComplx", ClientName)
+   # Setting up things
+   ClientName = 'Client_{}'.format(randint(1, 200))
+   print('This is {}'.format(ClientName))
+   git_repo = "https://github.com/DLTK/DLTK"
+   repo_dir = os.path.join("C:\\scalable\\", ClientName)
 
-    #create local copy of the repo
-    if not os.path.exists(repo_dir):
-        os.makedirs(repo_dir)
+   # create local copy of the repo
+   if not os.path.exists(repo_dir):
+       os.makedirs(repo_dir)
 
-    # if repo root is empty, fill it
-    if not os.listdir(repo_dir):
-        Repo.clone_from(git_repo, repo_dir)
-        repo = Repo(repo_dir)
-    else:
-        repo = Repo(repo_dir)
+   # if repo root is empty, fill it
+   if not os.listdir(repo_dir):
+       Repo.clone_from(git_repo, repo_dir)
+       repo = Repo(repo_dir)
+   else:
+       repo = Repo(repo_dir)
+   
+   results = {}
 
+   # Ask server for work
+   CCAddress = "http://localhost:4444"
+   while True:
+       start = datetime.datetime.now()
+       
+       # ask the server for a task
+       response = requests.get(CCAddress)
+       job = response.json()
 
+       if job == 'Done':
+           print ('No more jobs. Printing complexity and finish')
+           print (results) #dont need the complexity - server only cares about time
+           break
 
+       # CALCULATE THE COMPLEXITY
+       print('Client {} is calculating {}'.format(ClientName, job))
+       git = repo.git
+       git.checkout(job)
 
-    #Ask server for work
-    CCAddress = "http://localhost:4444"
-    while True:
-        # ask the server for a task
-        response = requests.get(CCAddress)
-        job = response.json()
-        
-        #if job == '"Done"':  # deal with the double quotes
-        #    break
+       # get a lit of py files to calculate complexity
+       files = update_files(repo_dir)
+       c = calculate_cc_complexity(files)
+       print('The complexity on commit {} was calculated by {}. Complexity value is: {}'.format(job, ClientName, c))
 
-        # CALCULATE THE COMPLEXITY
-        print('Client {} is calculating {}'.format(ClientName, job))
-        git = repo.git
-        git.checkout(job)
-        
-        #get a lit of py files to calculate complexity
-        files = update_files(repo_dir)
-        c = calculate_cc_complexity(files)
-        print ('The complexity on commit {} was calculated by {}. Complexity value is: {}'.format(job, ClientName, c))
+       #client stores the complexity
+       results[job] = c
+       
+       end  = datetime.datetime.now() 
+       totaltime = (end - start).microseconds
 
-        result = { 
-        'commit': job,
-        'complexity': str(c)
-        }
-  
-          
-        # post result to the server
-        #response = requests.post('http://localhost:4444', json='{}'.format(result))
-        response = requests.post('http://localhost:4444', json=result)
+       returnToServer = {'totaltime' : str(totaltime)}
+
+       # post result to the server
+       response = requests.post('http://localhost:4444', json=returnToServer)
 
 
