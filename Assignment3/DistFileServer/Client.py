471db7f69aebd0763d2f9fb9ecda09b97903b55e
Eokane <Eiokane@tcd.ie>
2017-12-07 21:20:32 +0000

    file server - create + open + update files

--- Assignment3/DistFileServer/Client.py
+++ Assignment3/DistFileServer/Client.py
@@ -1,27 +1,41 @@
-import random
-import dfs.api
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 21:19:19 2017
 
-if __name__ == '__main__':
+@author: Eibhlín
+"""
 
-    # File server
-    #requesting fies
-    file_1 = dfs.api.open('/etc/blub', 'w')
-    file_1.write(str(random.randint(0, 10000)) * 10)
-    file_1.close()
-
-    file_2 = dfs.api.open('/etc/blub', 'r')
-    read_content = file_2.read()
-    print("read content = ", read_content)
-    file_2.close()
+import requests
 
 
+FileServer = 'http://localhost:5555' 
 
-    # fs1
-    file_1 = dfs.api.open('/home/my_file2', 'w')
-    file_1.write(str(random.randint(0, 10000)) * 10)
-    file_1.close()
+############################ Create File
+fileName = 'file1'
+response = requests.post('{}/CreateFile/{}/'.format(FileServer, fileName), data="Line 1")
+print(response.text)
 
-    file_2 = dfs.api.open('/home/my_file2', 'r')
-    read_content = f2.read()
-    print("read content = ", read_content)
-    file_2.close()
+
+
+############################ list files
+print ('Files on the server:')
+requests.get("{}/ShowFiles".format(FileServer)).json()
+
+
+############################ Open 1 File
+#this is the name of the file you want to open:
+openFileName = 'file1'
+file = requests.get('{}/OpenFile/{}/'.format(FileServer, openFileName)).text 
+
+local_copy = open(openFileName , "w") 
+local_copy.write(file)
+
+############################ Change the file
+local_copy.write('\nChanging the file')
+local_copy.close()
+
+############################ Send it back to the server:
+#read the file again because it was closed
+local_copy = open(openFileName, "r")
+fileText = local_copy.read()
+response = requests.post('{}/CreateFile/{}/'.format(FileServer, openFileName), data=fileText)
